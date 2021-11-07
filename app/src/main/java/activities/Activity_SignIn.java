ac7fc9155f68ddba6a347fce51c2c561456b3916
nadav1996700 <nadav1996700@gmail.com>
2021-11-07 14:09:37 +0200

    fix battery function

--- app/src/main/java/activities/Activity_SignIn.java
+++ app/src/main/java/activities/Activity_SignIn.java
@@ -2,6 +2,7 @@ package activities;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.BatteryManager;
 import android.os.Bundle;
@@ -35,27 +36,18 @@ public class Activity_SignIn extends AppCompatActivity {
 
     // check credentials and move to actions page
     private void checkCredentials() {
-        if(password.getText() != null && !password.getText().toString().isEmpty() && checkBattery()
-            && checkBluetooth() && checkBrightness()) {
-            progressBar.setVisibility(View.VISIBLE);
-            if(!password.getText().toString().equals("123456")) {
-                message.setText(R.string.LoginFailed);
-                message.setTextColor(Color.RED);
-                message.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.GONE);
-            }
-            else {
-                message.setText(R.string.LoginSuccessfully);
-                message.setTextColor(Color.GREEN);
-                message.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.GONE);
-            }
+        progressBar.setVisibility(View.VISIBLE);
+        if(password.getText() != null && checkBattery() && checkBluetooth() && checkBrightness()
+            && checkOrientation()) {
+            message.setText(R.string.LoginSuccessfully);
+            message.setTextColor(Color.GREEN);
         }
         else {
             message.setText(R.string.LoginFailed);
             message.setTextColor(Color.RED);
-            message.setVisibility(View.VISIBLE);
         }
+        message.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
     }
 
     // check if Brightness value is less than 50
@@ -74,13 +66,21 @@ public class Activity_SignIn extends AppCompatActivity {
         } else return mBluetoothAdapter.isEnabled();
     }
 
-    // check if battery percentage is equal number
+    // check that orientation is portrait
+    private boolean checkOrientation() {
+        int orientation = getResources().getConfiguration().orientation;
+        boolean condition = orientation != Configuration.ORIENTATION_LANDSCAPE;
+        Log.d("pttt", "orientation: " + condition);
+        return orientation != Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    // check if password is equal to battery percentage
     private boolean checkBattery() {
         // Call battery manager service
         BatteryManager bm = (BatteryManager) context.getSystemService(BATTERY_SERVICE);
         // Get the battery percentage and store it in a INT variable
-        int batLevel = bm.getIntProperty(bm.BATTERY_PROPERTY_CAPACITY);
-        return batLevel % 2 == 0;
+        int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        return Integer.parseInt(password.getText().toString()) == batLevel;
     }
 
     // initialize variables
